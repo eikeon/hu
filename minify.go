076b34a8e4646de3fc7a2f7198a10e17e3a7a54a
Daniel Krech <eikeon@eikeon.com>
2011-07-12 20:04:53 -0400

    switched to inlining css (minified); removed left-right padding on image; fixed section Stylesheet start location

---
+++ minify.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"regexp"
+	"strings"
+)
+
+func minify(css string) string {
+	whitespace := regexp.MustCompile("[ \t\n\r\f\v]+")
+	css = whitespace.ReplaceAllString(css, " ")
+
+	comments := regexp.MustCompile("/\\*[^*]*\\*/")  // TODO
+	css = comments.ReplaceAllString(css, " ")
+
+	css = strings.Replace(css, "; ", ";", -1)
+	css = strings.Replace(css, ": ", ":", -1)
+	css = strings.Replace(css, " {", "{", -1)
+	css = strings.Replace(css, "{ ", "{", -1)
+	css = strings.Replace(css, ", ", ",", -1)
+	css = strings.Replace(css, "} ", "}", -1)
+	css = strings.Replace(css, ";}", "}", -1)
+	css = strings.TrimSpace(css)
+	return css
+}
