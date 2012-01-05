fdcec9fee019e2ad8e34d1122856e21a19b31b52
Daniel Krech <eikeon@eikeon.com>
2012-01-04 21:11:36 -0500

    turned ClosureIfNeeded into a method on Environment

--- environment.go
+++ environment.go
@@ -24,14 +24,6 @@ func NewEnvironment() *Environment {
 	return &Environment{frame: make(map[Symbol]Term)}
 }
 
-func ClosureIfNeeded(term Term, environment *Environment) Term {
-	switch v := term.(type) {
-	case Application:
-		return Closure{term, environment}
-	}
-	return term
-}
-
 // returns a new (child) environment from this environment extended
 // with bindings given by variables, values.
 func (environment *Environment) NewChildEnvironment() *Environment {
@@ -40,26 +32,34 @@ func (environment *Environment) NewChildEnvironment() *Environment {
 	return child
 }
 
+func (environment *Environment) Closure(term Term) Term {
+	switch v := term.(type) {
+	case Application:
+		return Closure{term, environment}
+	}
+	return term
+}
+
 func (environment *Environment) Extend(variables, values Term) {
 	for ; variables != nil && values != nil; variables, values = cdr(variables), cdr(values) {
 		switch variables.(type) {
 		case *Pair:
 			environment.Extend(car(variables), car(values))
 		default:
-			environment.frame[variables.(Symbol)] = ClosureIfNeeded(values, environment.parent)
+			environment.frame[variables.(Symbol)] = environment.parent.Closure(values)
 			return
 		}
 	}
 }
 
 func (environment *Environment) Define(variable Symbol, value Term) {
-	environment.frame[variable] = ClosureIfNeeded(value, environment)
+	environment.frame[variable] = environment.Closure(value)
 }
 
 func (environment *Environment) Set(variable Symbol, value Term) {
 	_, ok := environment.frame[variable]
 	if ok {
-		environment.frame[variable] = ClosureIfNeeded(value, environment)
+		environment.frame[variable] = environment.Closure(value)
 	} else if environment.parent != nil {
 		environment.parent.Set(variable, value)
 	} else {
